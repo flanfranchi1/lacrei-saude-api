92f2d432a3f1eae49448d20899f6039d99d6e795
Fernando Lanfranchi <felanfranchi@gmail.com>
2026-02-23 16:15:37 -0300

    feat: implement JWT authentication, CORS, and data validation

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework import serializers
 from .models import HealthProfessional, Appointment
 
@@ -6,6 +7,18 @@ class HealthProfessionalSerializer(serializers.ModelSerializer):
         model = HealthProfessional
         fields = ['id', 'social_name', 'profession', 'address', 'contact', 'is_active']
         read_only_fields = ['id']
+    
+    def validate_social_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Social name cannot be empty.")
+        return value
+    
+    def validate_contact(self, value):
+        clean_contact = re.sub(r'[^0-9]', '', value)
+
+        if len(clean_contact) not in (10,11):
+            raise serializers.ValidationError("Contact number must be in a valid Brazilian phone format (11 digits including area code).")
+        return value
 
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
--- setup/settings.py
+++ setup/settings.py
@@ -40,13 +40,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
+'rest_framework_simplejwt',
     "api",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -115,3 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
