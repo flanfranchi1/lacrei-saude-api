a91c038e6aca02f8221d88b3d205b020bb1341c8
Fernando Lanfranchi <felanfranchi@gmail.com>
2026-02-27 16:00:38 -0300

    fix: correct attribute name in appointment string representation

--- api/models.py
+++ api/models.py
@@ -4,14 +4,13 @@ from django.db import models
 
 
 class HealthProfessional(models.Model):
-
     class ProfessionChoices(models.TextChoices):
-        DOCTOR = 'Doctor', 'Médico(a)'
-        NURSE = 'Nurse', 'Enfermeiro(a)'
-        PSYCHOLOGIST = 'psychologist', 'Psicólogo(a)'
-        PHYSIOTHERAPIST = 'physiotherapist', 'Fisioterapeuta'
-        NUTRITIONIST = 'nutritionist', 'Nutricionista'
-        OTHER = 'other', 'Outro'
+        DOCTOR = "Doctor", "Médico(a)"
+        NURSE = "Nurse", "Enfermeiro(a)"
+        PSYCHOLOGIST = "psychologist", "Psicólogo(a)"
+        PHYSIOTHERAPIST = "physiotherapist", "Fisioterapeuta"
+        NUTRITIONIST = "nutritionist", "Nutricionista"
+        OTHER = "other", "Outro"
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     social_name = models.CharField(max_length=255)
@@ -27,10 +26,11 @@ class HealthProfessional(models.Model):
     def __str__(self):
         return self.name
 
+
 class Appointment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    health_professional = models.ForeignKey(HealthProfessional, on_delete=models.PROTECT, related_name='appointments')
+    health_professional = models.ForeignKey(HealthProfessional, on_delete=models.PROTECT, related_name="appointments")
     appointment_date = models.DateTimeField()
 
     def __str__(self):
-        return f"Appointment with {self.health_professional.social_name} on {self.date_time}"
+        return f"Appointment with {self.health_professional.social_name} on {self.appointment_date}"
